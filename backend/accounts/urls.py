b81929a6f732031eea8286c21b778a8cd9ebd0b6
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2024-12-12 06:18:44 -0300

    Logout no Django - Dica #16

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,10 @@
 # accounts/urls.py
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import include, path
+from .views import logout_view
 
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),  # noqa E501
+    path('logout/', logout_view, name='logout'),  # noqa E501
 ]
---
+++ backend/accounts/views.py
@@ -0,0 +1,7 @@
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.urls import reverse
+
+def logout_view(request):
+    logout(request)
+    return redirect('core:index')
